27ed0d2ac39109d5b0a0fd96eb133ed13a10e784
jessi68 <lyn990312@gmail.com>
2022-04-12 23:42:24 +0900

    feat: ui slot, quick slot

---
+++ Assets/Scripts/QuickSlotManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickSlotManager : MonoBehaviour
+{
+    [SerializeField] List<Sprite> _buttonIcons;
+
+    public Transform GridLayout;
+
+    public static QuickSlotButton CurrentlySelected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public static void AddToQuickSlot(QuickSlotButton quickslotButton)
+    {
+        // Äü½½·Ô µî·Ï
+        quickslotButton.ButtonImage.sprite = CurrentlySelected.ButtonImage.sprite;
+        quickslotButton.ButtonText.text = CurrentlySelected.ButtonText.text;
+
+        // ¹öÆÛ ºñ¿ì±â
+        CurrentlySelected = null;
+    }
+}
