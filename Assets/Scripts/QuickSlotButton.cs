213877d1b18da896c0fbc7cf408f696e42c1628b
Yeeun Grace Jo <gracejo0227@gmail.com>
2022-04-13 14:46:26 +0900

    [feat] Quickslot UI layout customize
    - 퀵슬롯 제작
    - 스크롤 가능한 그리드 레이아웃 제작
    - 닫기 버튼 추가

---
+++ Assets/Scripts/QuickSlotButton.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class QuickSlotButton : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerClickHandler
+{
+    public enum _ButtonType {Quickslot, Viewport}
+
+    public _ButtonType ButtonType;
+    public Image ButtonImage; 
+    public Text ButtonText;
+    public int fid;
+
+
+    public void OnSelect(BaseEventData eventData) 
+    {
+        if (ButtonType == _ButtonType.Viewport)
+        {
+            QuickSlotManager.CurrentlySelected = this;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(ButtonType == _ButtonType.QuickSlot && QuickSlotManager.CurrentlySelected != null)
+        {
+
+        }
+    }
+
+    void Start()
+    {
+        QuickSlotButton buttonProp;
+        
+        for (int i = 0; i < GridLayout.childCount; i++)
+        {
+            buttonPop = GridLayout.GetChild(i).GetComponent<QuickSlotButton>();
+            buttonProp.ButtonImage.sprite = _buttonIcons[i];
+            buttonProp.ButtonText.text = _buttonIcons[i].name;
+            buttonProp.fid = i;
+        }
+    }
+
+    public static void AddToQuickSlot(QuickSlotButton quickslotButton)
+    {
+        quickslotButton.ButtonImage.sprite = CurrentlySelected.ButtonImage.sprite;
+        quickslotButton.ButtonText.text = CurrentlySelected.ButtonText.text;
+        quickslotButton.fid = CurrentlySelected.fid;
+
+        CurrentlySelected = null;
+    }
+    // not registered: true, registerd: false
+    static int CheckDistinct()
+    {
+        return -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
