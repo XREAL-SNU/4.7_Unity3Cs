be42609f47b0ab746189ecfb3276e21f59abe319
Yeeun Grace Jo <gracejo0227@gmail.com>
2022-04-13 21:13:36 +0900

    [feat] quickslot face position change
    - change position
    - make track of already added face (use list)

--- Assets/Scripts/QuickSlotButton.cs
+++ Assets/Scripts/QuickSlotButton.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class QuickSlotButton : MonoBehaviour, ISelectHandler
+public class QuickSlotButton : MonoBehaviour, ISelectHandler, IPointerClickHandler
 {
     public enum _ButtonType {Quickslot, Viewport}
 
     public _ButtonType ButtonType;
     public Image ButtonImage; 
     public Text ButtonText;
+    public int fid;
+
     
     public void OnSelect(BaseEventData eventData) 
     {
-        
+         if (ButtonType == _ButtonType.Viewport)
+        {
+            QuickSlotManager.CurrentlySelected = this;
+            //Debug.Log(QuickSlotManager.CurrentlySelected );
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(ButtonType == _ButtonType.Quickslot && QuickSlotManager.CurrentlySelected != null)
+        {
+            QuickSlotManager.AddToQuickSlot(this) ;
+            //Debug.Log(QuickSlotManager.CurrentlySelected );
+        }
     }
 }
--- Assets/Scripts/QuickSlotManager.cs
+++ Assets/Scripts/QuickSlotManager.cs
@@ -7,11 +7,14 @@ using UnityEngine.EventSystems;
 public class QuickSlotManager : MonoBehaviour
 {
    [SerializeField] List<Sprite> _buttonIcons;
+   [SerializeField] List<QuickSlotButton> _quickSlots;
+
 
    public Transform GridLayout; // 버튼 한꺼번에 참조 -> Transform.GetChild()  
    public GameObject _viewportButtonPrefab;
-
-
+   
+   public static QuickSlotButton CurrentlySelected; 
+   public static List<QuickSlotButton> s_quickSlots;
 
     void Start()
     {
@@ -22,15 +25,79 @@ public class QuickSlotManager : MonoBehaviour
         for(int i = 0; i < _buttonIcons.Count; i++)
         {
             // Instantiate + GetComponent
-            buttonInstance = Instantiate(_viewportButtonPrefab, GridLayout); //
-            buttonProp = buttonInstance.GetComponent<QuickSlotButton>(); //
+            buttonInstance = (GameObject)Instantiate(_viewportButtonPrefab, GridLayout); //
+            buttonProp = buttonInstance.GetComponent<QuickSlotButton>(); // refer to QuickSlotManager
 
             // Set QuickSortButton Properties
             buttonProp = GridLayout.GetChild(i).GetComponent<QuickSlotButton>();
             buttonProp.ButtonImage.sprite = _buttonIcons[i];
             buttonProp.ButtonText.text = _buttonIcons[i].name;
+            buttonProp.fid = i;
+        }
+
+        s_quickSlots = new List<QuickSlotButton>();
+        for (int i = 0; i < _quickSlots.Count; i++)
+        {
+            s_quickSlots.Add(_quickSlots[i]);
         }
 
 
     }
+
+    public static void AddToQuickSlot(QuickSlotButton quickslotButton)
+    {
+       int index = CheckDistinct();
+       
+       if(index == -1)
+       {
+           //Debug.Log(CurrentlySelected.fid);
+           // register quickslot
+            quickslotButton.ButtonImage.sprite = CurrentlySelected.ButtonImage.sprite;
+            quickslotButton.ButtonText.text = CurrentlySelected.ButtonText.text;
+            quickslotButton.fid = CurrentlySelected.fid;
+       }        
+       else
+       {
+           SwapButtons(quickslotButton, index);
+       }
+
+        // vacate buffer
+        CurrentlySelected = null;
+    }
+
+    static int CheckDistinct() 
+    {
+        int retVal = -1; // not regsitered && not overlapped => -1
+
+        for (int i = 0; i < s_quickSlots.Count; i++)
+        {
+            if(s_quickSlots[i].fid == CurrentlySelected.fid)
+                retVal = i;
+        }
+        //Debug.Log("list length: " + s_quickSlots.Count);
+        return retVal;
+    }
+
+    static void SwapButtons(QuickSlotButton quickslotButton, int index)
+    {
+        Sprite spriteBuf = quickslotButton.ButtonImage.sprite;
+        string textBuf = quickslotButton.ButtonText.text; 
+        int idBuf = quickslotButton.fid;
+
+        quickslotButton.ButtonImage.sprite = s_quickSlots[index].ButtonImage.sprite;
+        quickslotButton.ButtonText.text = s_quickSlots[index].ButtonText.text;
+        quickslotButton.fid = s_quickSlots[index].fid;
+
+        s_quickSlots[index].ButtonImage.sprite = spriteBuf;
+        s_quickSlots[index].ButtonText.text = textBuf;
+        s_quickSlots[index].fid = idBuf;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // vacate buffer
+        CurrentlySelected = null;
+    }
+
+
 }
