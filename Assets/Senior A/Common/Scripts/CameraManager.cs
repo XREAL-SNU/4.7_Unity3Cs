b07eeeef55f158a947f12a0ac38034d846e932a3
p4r4dox82 <p4r4dox82@snu.ac.kr>
2022-04-14 14:04:04 +0900

    [feat] ThirdPersonCamera
    3인칭 카메라 구현

---
+++ Assets/Senior A/Common/Scripts/CameraManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    private GameObject player;
+    private CinemachineFreeLook thirdPersonCamera;
+    private GameObject playerLookAt;
+    private GameObject playerFollow;
+
+    private bool isMouseRotate = false;
+    private float xRotateSpeed = 300f;
+    private float yRotateSpeed = 2f;
+
+    private float zoomspeed = 2f;
+
+    public void Start() {
+        player = GameObject.FindWithTag("Player").gameObject;
+        thirdPersonCamera = GameObject.Find("ThirdPersonCamera").GetComponent<CinemachineFreeLook>();
+        playerLookAt = player.transform.Find("LookAtPoint").gameObject;
+        thirdPersonCamera.Follow = player.transform;
+        thirdPersonCamera.LookAt = playerLookAt.transform;
+    }
+
+    public void Update() {
+        if(Input.GetMouseButtonDown(1)) {
+            isMouseRotate = true;
+        }
+        if(Input.GetMouseButtonUp(1)) {
+            isMouseRotate = false;
+        }
+    }
+
+    public void LateUpdate() {
+        RotateLookAt(isMouseRotate);
+        Zoom(Input.mouseScrollDelta.y);
+    }
+
+    private void RotateLookAt(bool isMouseRotate) {
+        float _xRotateSpeed = 0f;
+        float _yRotateSpeed = 0f;
+        if(isMouseRotate) {
+            _xRotateSpeed = xRotateSpeed;
+            _yRotateSpeed = yRotateSpeed;
+        }
+        thirdPersonCamera.m_XAxis.m_MaxSpeed = _xRotateSpeed;
+        thirdPersonCamera.m_YAxis.m_MaxSpeed = _yRotateSpeed;
+    }
+
+    private void Zoom(float zoomDirection) {
+        if(zoomDirection > 0) {
+            thirdPersonCamera.m_Lens.FieldOfView = Mathf.Min(thirdPersonCamera.m_Lens.FieldOfView + zoomspeed, 50);
+        } else if(zoomDirection < 0) {
+            thirdPersonCamera.m_Lens.FieldOfView = Mathf.Max(thirdPersonCamera.m_Lens.FieldOfView - zoomspeed, 5);
+        }
+    }
+}
